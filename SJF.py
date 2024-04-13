5de73e376ffc1f79eca24d66f91bfe5e0f200382
Waleed Alzamil <95117901+WaleedAlzamil80@users.noreply.github.com>
2024-04-13 21:17:09 +0200

    process and sjf file added

--- SJF.py
+++ SJF.py
@@ -1,13 +1,4 @@
-class Process:
-    def __init__(self, pid, arrival_time, burst_time):
-        self.pid = pid
-        self.arrival_time = arrival_time
-        self.burst_time = burst_time
-        self.completion_time = 0
-        self.turnaround_time = 0
-        self.waiting_time = 0
-        self.original_burst_time = burst_time
-        self.done = False
+from process import *
 
 def sjf_non_preemptive(processes):
     processes.sort(key=lambda process: process.burst_time)
---
+++ process.py
@@ -0,0 +1,10 @@
+class Process:
+    def __init__(self, pid, arrival_time, burst_time):
+        self.pid = pid
+        self.arrival_time = arrival_time
+        self.burst_time = burst_time
+        self.completion_time = 0
+        self.turnaround_time = 0
+        self.waiting_time = 0
+        self.original_burst_time = burst_time
+        self.done = False
