5147453af66efd94fcb9ffbb9cb5eba4fbbeb57d
Mohamed Rezk <mohrizq895@gmail.com>
2024-04-16 21:43:45 +0200

    Implement Main Menu and Interface with gui

---
+++ main.py
@@ -0,0 +1,21 @@
+from menu import * 
+from gui import GUIInterface
+
+graphical_interface = GUIInterface(scheduler_window)
+
+def game_loop():
+    while True:
+        DISPLAY.fill("#252525")
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                exit()
+        if main_menu.is_enabled():
+            main_menu.update(events)
+            main_menu.draw(DISPLAY)
+
+        if graphical_interface._menu.is_enabled():
+            graphical_interface.start_update_loop(events)
+
+        pygame.display.flip()
+game_loop()
---
+++ menu.py
@@ -0,0 +1,51 @@
+import pygame
+import pygame_menu as pgm
+
+from themes import *
+
+pygame.init()
+
+WIDTH = 1280
+HEIGHT = 720
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (211, 211, 211)
+# limit FPS to 30 fps
+FPS = 30
+DISPLAY = pygame.display.set_mode((WIDTH, HEIGHT))
+
+scheduler_window = pgm.Menu(
+    title="",
+    width=WIDTH,
+    height=HEIGHT,
+    theme=game_theme,
+    enabled=False,
+    mouse_motion_selection=True,
+    center_content=False,
+    overflow=False,
+    position=(0, 0),
+)
+
+
+main_menu = pgm.Menu(
+    title="CPU Scheduler",
+    width=WIDTH,
+    height=HEIGHT,
+    theme=main_menu_theme,
+    mouse_motion_selection=True,
+    onclose=pgm.events.EXIT,
+    position=(0, 0, False),
+    verbose=False,
+)
+
+play_button = main_menu.add.button("Start Scheduling", scheduler_window)
+
+mode_selector = main_menu.add.selector(
+    "Algorithm ",
+    [("FCFS", 0), ("SJF-Non-Preemptive", 1), ("SJF-Preemptive", 2),  ("Round Robin", 3)],
+    onchange=None,
+)
+
+quit_button = main_menu.add.button("Quit", pgm.events.EXIT)
+
